72a67cb9c1eee92ec2e2cd97a7c60fda674f8956
DelicateIntegral <172169645+DelicateIntegral@users.noreply.github.com>
2024-06-19 11:03:29 +0530

    clean up

--- clean.sh
+++
@@ -1,7 +0,0 @@
-# Apply fix patches
-python3 -m cyoa.tools.client project.patch --project "$1" \
-  --patch cyoa.patch:FixScoreLabels \
-          cyoa.patch:FixActiveFlags \
-          cyoa.patch:FixConditionLabels \
-          cyoa.patch:ClearEditingFlag \
-          cyoa.patch:FixMultiSelectCounters \
--- cyoa/tools/wiki/config/defaults.py
+++
@@ -1,9 +0,0 @@
-DEFAULT_SECTION = {
-    "columns": 3,
-    "group_by_row": False,
-    "skip_row_title": []
-}
-DEFAULT_SECTION_BY_ROW = {
-    **DEFAULT_SECTION,
-    "group_by_row": True,
-}
